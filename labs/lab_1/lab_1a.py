f7a765a4749e7dea1c0b68dce2a85f97ec3ad46d
Ryan Jiang <ryanbjiang12+github@gmail.com>
2026-03-28 16:28:32 -0400

    Second print test
    Just an addition...

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Ryan Jiang" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Second...")
 
 if __name__ == "__main__":
     main()
